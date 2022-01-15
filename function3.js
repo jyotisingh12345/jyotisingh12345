abd5e70c903f58ae3ed15c8231c76c7c0952f520
jyoti singh <erjyotisingh02@gmail.com>
2022-01-15 11:12:17 +0530

    function3.js

---
+++ function3.js
@@ -0,0 +1,11 @@
+var a=200;
+var b=200;
+var c=300;
+if (a > b && a > c)
+console.log('fisrt is greater than second one');
+ else if (a < b && b < c)
+console.log('first and second are equal');
+else if (a=b=c)
+console.log('a and b are equal');
+else 
+console.log('c is greater');
